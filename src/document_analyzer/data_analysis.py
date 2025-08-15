f75f028036635dfca1cb0593313934a70150bf42
Aniruddha Mohanty <aniruddha.mohanty@gmail.com>
2025-08-15 20:05:54 +0530

    updated code

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,13 +1,12 @@
-
 import os
 import sys
 from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
-from exception.custom_exception_archieve import DocumentPortalException
+from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
-from prompt.prompt_library import PROPMT_REGISTRY
+from prompt.prompt_library import PROMPT_REGISTRY # type: ignore
 
 class DocumentAnalyzer:
     """
@@ -24,7 +23,7 @@ class DocumentAnalyzer:
             self.parser = JsonOutputParser(pydantic_object=Metadata)
             self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
             
-            self.prompt = PROPMT_REGISTRY["document_analysis"]
+            self.prompt = PROMPT_REGISTRY["document_analysis"]
             
             self.log.info("DocumentAnalyzer initialized successfully")
             
@@ -56,3 +55,5 @@ class DocumentAnalyzer:
         except Exception as e:
             self.log.error("Metadata analysis failed", error=str(e))
             raise DocumentPortalException("Metadata extraction failed",sys)
+        
+    
